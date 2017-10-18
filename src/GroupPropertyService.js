78d04ee103270a5eb4fc3b91eeaf3b87e427a597
Jeff Dufresne <jeffery.dufresne@gmail.com>
2017-10-18 18:36:59 -0400

    finished initial group property service. added Menu component and modified App component to use Menu component. Menu component just spits out group names at the moment.

---
+++ src/GroupPropertyService.js
@@ -0,0 +1,95 @@
+const groupPropertyCache = {
+    'General Info': []
+};
+
+const groupDisplayNames = {
+    'address': 'Address',
+    'education': 'Education',
+    'email': 'Email',
+    'employment': 'Employment',
+    'giving_annual_donation': 'Giving Annual Donation',
+    'phone': 'Phone',
+    'avatar': 'Avatar',
+    'facebook': 'Facebook',
+    'giving': 'Giving'
+};
+
+//do I need to define order too?
+/*const generalInfoDisplayProperties = {
+    'deceased': 'Deceased',
+    'dt_deceased': 'Date Deceased',
+    'ethnicity': 'Ethnicity',
+    'gender': 'Gender',
+    'name_first': 'First Name',
+    'name_last': 'Last Name',
+    'name_maiden': 'Previous Last Name',
+    'name_middle': 'Middle Name',
+    'name_nick': 'Nickname',
+    'name_prefix': 'Prefix',
+    'name_suffix': 'Suffix',
+    'year': 'Year'
+};*/
+
+
+
+async function loadJson() {
+    try {
+        return (await fetch(process.env.PUBLIC_URL + '/schema.json')).json();
+    } catch (e) {
+        console.error('fetch schema.json exception:');
+    }
+}
+
+function transformSchema(schema) {
+    
+   schema.forEach(schemaEntry => {
+        if(isGeneralProperty(schemaEntry)) {
+            groupPropertyCache['General Info'].push(createTransformedEntry(schemaEntry))
+        } else if(hasContainingObject(schemaEntry)) {
+            groupPropertyCache[groupDisplayNames[schemaEntry.containing_object.name]] = [];
+            schemaEntry.containing_object.properties.forEach(property => {
+                groupPropertyCache[groupDisplayNames[schemaEntry.containing_object.name]].push(createTransformedEntry(property));
+            });
+        } else if(hasPropertiesObject(schemaEntry)) {
+            groupPropertyCache[groupDisplayNames[schemaEntry.name]] = [];
+            schemaEntry.properties.forEach(property => {
+                groupPropertyCache[groupDisplayNames[schemaEntry.name]].push(createTransformedEntry(property));
+            });
+        }
+    });
+
+    return schema;
+
+    function isGeneralProperty(schemaEntry) {
+        return !hasContainingObject(schemaEntry) && !hasPropertiesObject(schemaEntry); //Giving and a few others doesn't have containing object but does have properties object
+    }
+    function hasContainingObject(schemaEntry) {
+        return schemaEntry.hasOwnProperty('containing_object');
+    }
+    function hasPropertiesObject(schemaEntry) {
+        return schemaEntry.hasOwnProperty('properties');
+    }
+    function createTransformedEntry(schemaEntry) {
+        return {
+            dataType: schemaEntry.data_type,
+            appKeys: schemaEntry.app_keys,
+            name: schemaEntry.name
+        }
+    }
+}
+
+async function getGroups() {
+    if(groupPropertyCache['General Info'].length === 0) {
+        transformSchema(await loadJson());
+    }
+    return Object.getOwnPropertyNames(groupPropertyCache);
+}
+
+function getGroupProperties(group) {
+    return groupPropertyCache[group];
+}
+
+export default {
+    getGroups: getGroups,
+    getGroupProperties: getGroupProperties
+}
---
+++ src/Menu.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import GroupPropertyService from './GroupPropertyService'
+
+class Menu extends Component {
+    componentDidMount() {
+        GroupPropertyService.getGroups()
+            .then(groups => {
+                this.setState({groups: groups});
+            });
+    }
+    render() {
+        let groups;
+        if(this.state && this.state.groups) {
+            groups = this.state.groups.map(group => <li key={group}>{group}</li>)
+        }
+        return (
+            <ul>
+                {groups}
+            </ul>
+        );
+    }
+  }
+  
+  export default Menu;
