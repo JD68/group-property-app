a798719e3f18a2087705251269ba7b54edbb2e18
Jeff Dufresne <jeffery.dufresne@gmail.com>
2017-10-20 14:25:22 -0400

    added scroll to group properties panel

--- src/GroupProperties.js
+++ src/GroupProperties.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Panel /*, ListGroup, ListGroupItem*/} from 'react-bootstrap';
+import { Panel } from 'react-bootstrap';
+import './GroupProperties.css';
 
 class GroupProperties extends Component {
     /*constructor(props) {
@@ -13,18 +14,21 @@ class GroupProperties extends Component {
         if(!this.props.group) {
             return (<h1>Please Select A Group</h1>);
         } else {
-            return (<Panel header={this.props.group}>
-                <div>{this.props.property}</div>
-                {
-                    this.props.properties.map(property => {
-                        return <Panel header={property.displayName} key={this.props.group + property.name}>
-                            <div>Type: {property.dataType}</div>
-                            <div>Usage: {property.appKeys.join(',')}</div>
-                            <div>EverTrue Name: {property.name}</div>
-                        </Panel>;
-                    })
-                }
-            </Panel>);
+            return (
+                <div className="group-properties-panel">
+                    <div className="well"><h5>{this.props.group}</h5></div>
+                    <div className="group-properties-groups">
+                    {
+                        this.props.properties.map(property => {
+                            return <Panel header={property.displayName} key={this.props.group + property.name}>
+                                <div>Type: {property.dataType}</div>
+                                <div>Usage: {property.appKeys.join(',')}</div>
+                                <div>EverTrue Name: {property.name}</div>
+                            </Panel>;
+                        })
+                    }
+                    </div>
+                </div>)
         }
     }
   }
